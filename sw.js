18c78f3edafa0ecade1c104d191a369486f0bcd8
大埜健 <macbookpro2019@MacBook-Pro-M3-Pro.local>
2026-03-04 03:05:25 +0900

    Fix seek bar alignment to center on tracks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = new URL(self.location).searchParams.get('v') || '2.120.4';
+const VERSION = new URL(self.location).searchParams.get('v') || '2.120.5';
 const CACHE_NAME = 'music-player-v' + VERSION;
 const ASSETS = [
     './',
