f669a36ec3211498d306305529cc9dfdf4b9c308
大埜健 <macbookpro2019@MacBook-Pro-M3-Pro.local>
2026-03-04 03:40:06 +0900

    Fix seek slider marker alignment by natively shifting marker containers inwards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = new URL(self.location).searchParams.get('v') || '2.120.6';
+const VERSION = new URL(self.location).searchParams.get('v') || '2.120.7';
 const CACHE_NAME = 'music-player-v' + VERSION;
 const ASSETS = [
     './',
